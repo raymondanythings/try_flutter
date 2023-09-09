d456dd0ae5cb0e4c22b704ad1b44895b384b3ac3
Raymond <akdfhr2@gmail.com>
2023-09-09 21:28:36 +0900

    [230829-D5] upload images

---
+++ d2/lib/features/feeds/image_upload_screen.dart
@@ -0,0 +1,247 @@
+import 'package:camera/camera.dart';
+import 'package:d2/constants/gaps.dart';
+import 'package:d2/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ImageUploadScreen extends StatefulWidget {
+  const ImageUploadScreen({super.key});
+
+  @override
+  State<ImageUploadScreen> createState() => _ImageUploadScreenState();
+}
+
+class _ImageUploadScreenState extends State<ImageUploadScreen> {
+  bool _hasPermission = false;
+  bool _isSelfieMode = false;
+  bool _isFlashOn = false;
+
+  late CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) {
+      return;
+    }
+    _cameraController = CameraController(
+      cameras[_isSelfieMode ? 1 : 0],
+      ResolutionPreset.ultraHigh,
+    );
+
+    await _cameraController.initialize();
+
+    await _cameraController.prepareForVideoRecording();
+
+    setState(() {});
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      initCamera();
+    }
+  }
+
+  Future<void> _toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    initCamera();
+  }
+
+  Future<void> _toggleFlash() async {
+    await _cameraController
+        .setFlashMode(_isFlashOn ? FlashMode.off : FlashMode.torch);
+    setState(() {
+      _isFlashOn = !_isFlashOn;
+    });
+  }
+
+  Future<void> _captureImage() async {
+    final picture = await _cameraController.takePicture();
+    if (picture == null) return;
+    _onUploadImages([picture]);
+  }
+
+  Future<void> _onPickImagePress() async {
+    final pictures = await ImagePicker().pickMultiImage();
+    if (pictures.isEmpty) return;
+    if (!mounted) return;
+    _onUploadImages(pictures);
+  }
+
+  void _onUploadImages(List<XFile> pictures) {
+    Navigator.of(context).pop(pictures);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: !_hasPermission || !_cameraController.value.isInitialized
+              ? const Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Initializing...",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                    Gaps.v20,
+                    CircularProgressIndicator.adaptive(),
+                  ],
+                )
+              : Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        clipBehavior: Clip.hardEdge,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(
+                              Sizes.size40,
+                            ),
+                            bottomRight: Radius.circular(
+                              Sizes.size40,
+                            ),
+                          ),
+                        ),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            CameraPreview(_cameraController),
+                            Positioned(
+                              top: MediaQuery.of(context).viewPadding.top +
+                                  Sizes.size8,
+                              left: Sizes.size32,
+                              child: GestureDetector(
+                                onTap: () => Navigator.of(context).pop(),
+                                child: const FaIcon(
+                                  FontAwesomeIcons.chevronLeft,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: Sizes.size28,
+                              width: MediaQuery.of(context).size.width,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  GestureDetector(
+                                    onTap: _toggleFlash,
+                                    child: FaIcon(
+                                      _isFlashOn
+                                          ? Icons.flash_on
+                                          : Icons.flash_off,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      const SizedBox(
+                                        width: Sizes.size60 + Sizes.size14,
+                                        height: Sizes.size60 + Sizes.size14,
+                                        child: CircularProgressIndicator(
+                                          color: Colors.white,
+                                          strokeWidth: Sizes.size4,
+                                          value: 1,
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: _captureImage,
+                                        child: Container(
+                                          width: Sizes.size60,
+                                          height: Sizes.size60,
+                                          decoration: const BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  IconButton(
+                                    onPressed: _toggleSelfieMode,
+                                    icon: const FaIcon(
+                                      FontAwesomeIcons.rotate,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: Sizes.size96 + Sizes.size20,
+                      child: Row(
+                        children: [
+                          const Spacer(),
+                          const Text(
+                            "Camera",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: Sizes.size16,
+                            ),
+                          ),
+                          Expanded(
+                            child: Center(
+                              child: GestureDetector(
+                                onTap: _onPickImagePress,
+                                child: Text(
+                                  "Library",
+                                  style: TextStyle(
+                                    color: Colors.grey.shade700,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: Sizes.size16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
--- d2/lib/features/feeds/widgets/write_feed.dart
+++ d2/lib/features/feeds/widgets/write_feed.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:d2/constants/gaps.dart';
 import 'package:d2/constants/sizes.dart';
+import 'package:d2/features/feeds/image_upload_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -15,6 +19,7 @@ class _WriteFeedState extends State<WriteFeed> {
     Navigator.of(context).pop();
   }
 
+  List<XFile> pictures = [];
   late TextEditingController _inputController;
   late ScrollController _scrollContoller;
 
@@ -33,6 +38,23 @@ class _WriteFeedState extends State<WriteFeed> {
     setState(() {
       numLines = lines > 3 ? lines : 3;
     });
+  }
+
+  void _onUploadTap() async {
+    List<XFile>? picturesResult = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ImageUploadScreen(),
+      ),
+    );
+    if (picturesResult == null || picturesResult.isEmpty) return;
+    pictures = picturesResult;
+    setState(() {});
+  }
+
+  void _removePicture(int index) {
+    if (pictures.isEmpty) return;
+    pictures.removeAt(index);
+    setState(() {});
   }
 
   @override
@@ -95,140 +117,188 @@ class _WriteFeedState extends State<WriteFeed> {
             ),
           ),
         ),
-        body: Container(
-          padding: const EdgeInsets.all(
-            Sizes.size16,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Scrollbar(
-                controller: _scrollContoller,
-                child: SingleChildScrollView(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(
+              Sizes.size16,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Scrollbar(
                   controller: _scrollContoller,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        fit: FlexFit.tight,
-                        child: Column(
-                          children: [
-                            const CircleAvatar(
-                              foregroundImage: NetworkImage(
-                                "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png",
-                              ),
-                            ),
-                            Gaps.v14,
-                            AnimatedContainer(
-                              duration: const Duration(
-                                milliseconds: 300,
-                              ),
-                              width: 2,
-                              height: numLines * 20 + 14,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade400,
-                              ),
-                            ),
-                            Gaps.v14,
-                            const Opacity(
-                              opacity: 0.4,
-                              child: CircleAvatar(
-                                radius: Sizes.size10,
+                  child: SingleChildScrollView(
+                    controller: _scrollContoller,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          fit: FlexFit.tight,
+                          child: Column(
+                            children: [
+                              const CircleAvatar(
                                 foregroundImage: NetworkImage(
                                   "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png",
                                 ),
                               ),
-                            ),
-                          ],
+                              Gaps.v14,
+                              AnimatedContainer(
+                                duration: const Duration(
+                                  milliseconds: 300,
+                                ),
+                                width: 2,
+                                height: numLines * 20 +
+                                    14 +
+                                    (pictures.isEmpty ? 0 : 100),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade400,
+                                ),
+                              ),
+                              Gaps.v14,
+                              const Opacity(
+                                opacity: 0.4,
+                                child: CircleAvatar(
+                                  radius: Sizes.size10,
+                                  foregroundImage: NetworkImage(
+                                    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png",
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Flexible(
+                          flex: 5,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Raymond",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Gaps.v10,
+                              TextField(
+                                autocorrect: false,
+                                enableSuggestions: false,
+                                controller: _inputController,
+                                onChanged: _onChangeNumLInes,
+                                minLines: 1,
+                                maxLines: null,
+                                style: const TextStyle(
+                                  fontSize: Sizes.size12,
+                                ),
+                                cursorColor: Colors.blue[800],
+                                decoration: const InputDecoration(
+                                  hintText: "Start a thread...",
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: Sizes.size1,
+                                  ),
+                                  border: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                  ),
+                                ),
+                              ),
+                              if (pictures.isNotEmpty)
+                                Container(
+                                  padding: const EdgeInsets.only(
+                                    bottom: Sizes.size10,
+                                  ),
+                                  height: 128,
+                                  child: ListView.separated(
+                                    scrollDirection: Axis.horizontal,
+                                    shrinkWrap: true,
+                                    itemCount: pictures.length,
+                                    itemBuilder: (context, index) {
+                                      return Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(
+                                            Sizes.size12,
+                                          ),
+                                        ),
+                                        clipBehavior: Clip.hardEdge,
+                                        height: 128,
+                                        child: Stack(
+                                          children: [
+                                            Image.file(
+                                              File(pictures[index].path),
+                                              fit: BoxFit.contain,
+                                            ),
+                                            Positioned(
+                                              right: Sizes.size2,
+                                              top: Sizes.size2,
+                                              child: GestureDetector(
+                                                onTap: () =>
+                                                    _removePicture(index),
+                                                child: FaIcon(
+                                                  FontAwesomeIcons
+                                                      .solidCircleXmark,
+                                                  color: Colors.grey.shade400,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                    separatorBuilder: (context, index) {
+                                      return Gaps.h10;
+                                    },
+                                  ),
+                                ),
+                              GestureDetector(
+                                onTap: _onUploadTap,
+                                child: const FaIcon(
+                                  FontAwesomeIcons.paperclip,
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Sizes.size10,
+                    vertical: Sizes.size20,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Anyone can reply",
+                        style: TextStyle(
+                          color: Colors.grey.shade400,
+                          fontSize: Sizes.size16 + Sizes.size2,
                         ),
                       ),
-                      Flexible(
-                        flex: 5,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              "Raymond",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Gaps.v10,
-                            TextField(
-                              autocorrect: false,
-                              enableSuggestions: false,
-                              controller: _inputController,
-                              onChanged: _onChangeNumLInes,
-                              minLines: 1,
-                              maxLines: null,
-                              style: const TextStyle(
-                                fontSize: Sizes.size12,
-                              ),
-                              cursorColor: Colors.blue[800],
-                              decoration: const InputDecoration(
-                                hintText: "Start a thread...",
-                                contentPadding: EdgeInsets.symmetric(
-                                  vertical: Sizes.size1,
-                                ),
-                                border: UnderlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                ),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              height: 40,
-                              padding: const EdgeInsets.symmetric(
-                                vertical: Sizes.size20,
-                              ),
-                              child: const FaIcon(
-                                FontAwesomeIcons.paperclip,
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
+                      AnimatedOpacity(
+                        opacity: _inputController.text.isNotEmpty ? 1 : 0.6,
+                        duration: const Duration(
+                          milliseconds: 200,
+                        ),
+                        child: Text(
+                          "Post",
+                          style: TextStyle(
+                            color: Colors.blue[700],
+                            fontSize: Sizes.size16 + Sizes.size2,
+                          ),
                         ),
                       )
                     ],
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size10,
-                  vertical: Sizes.size20,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Anyone can reply",
-                      style: TextStyle(
-                        color: Colors.grey.shade400,
-                        fontSize: Sizes.size16 + Sizes.size2,
-                      ),
-                    ),
-                    AnimatedOpacity(
-                      opacity: _inputController.text.isNotEmpty ? 1 : 0.6,
-                      duration: const Duration(
-                        milliseconds: 200,
-                      ),
-                      child: Text(
-                        "Post",
-                        style: TextStyle(
-                          color: Colors.blue[700],
-                          fontSize: Sizes.size16 + Sizes.size2,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
         backgroundColor: Colors.white,
